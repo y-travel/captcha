c4c82cb8f8e4e6c551e4741647905954cad99ad7
Mr. Happiness <m.farahmandzad@outlook.com>
2024-03-31 13:31:44 +0330

    feat: add custom letters parameter to captcha function

--- test/index.js
+++ test/index.js
@@ -7,3 +7,9 @@ test('should return an object and include token and buffer', async t => {
   t.is(token.length, 5)
   t.is(buffer.length, 17646)
 })
+test('random numbers should be from letters', async t => {
+	const { token } = await captcha({letters:'1'})
+	console.log(token);
+	t.is(token, '11111')
+  })
+  
